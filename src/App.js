c2cb41417cce75991f937576502950a3bee88618
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-08 21:10:20 +0800

    Add Button component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Button from './Button'
 
 function App() {
   return (
@@ -10,17 +11,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <Button>Toggle</Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
