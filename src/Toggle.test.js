68058062d30d8a7d21c6986198608e231cc1053b
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-13 19:08:55 +0800

    Assert state

--- src/Toggle.test.js
+++ src/Toggle.test.js
@@ -9,12 +9,14 @@ configure({ adapter: new Adapter() })
 describe('<Toggle />', () => {
   it('should render Button component', () => {
     const wrapper = shallow(<Toggle />)
-    console.log(wrapper.debug())
+    // console.log(wrapper.debug())
 
+    expect(wrapper.state().toggle).toBe(false)
     expect(wrapper.find('span').prop('children')).toBe('Off')
 
     wrapper.find(Button).simulate('click')
 
+    expect(wrapper.state().toggle).toBe(true)
     expect(wrapper.find('span').prop('children')).toBe('On')
   })
 })
