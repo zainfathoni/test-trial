81da4a2a02b8c87c951a230c737578da08350608
Zain Fathoni <zain.fathoni@gmail.com>
2019-07-13 19:29:36 +0800

    Test Toggle using React Testing Library

--- src/Toggle.test.js
+++ src/Toggle.test.js
@@ -1,13 +1,14 @@
-import React from 'react'
 import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import Toggle from './Toggle'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import React from 'react'
 import Button from './Button'
+import Toggle from './Toggle'
 
 configure({ adapter: new Adapter() })
 
-describe('<Toggle />', () => {
-  it('should render Button component', () => {
+describe('Test Toggle using Enzyme', () => {
+  it('shallow render Button component', () => {
     const wrapper = shallow(<Toggle />)
     // console.log(wrapper.debug())
 
@@ -18,5 +19,21 @@ describe('<Toggle />', () => {
 
     expect(wrapper.state().toggle).toBe(true)
     expect(wrapper.find('span').prop('children')).toBe('On')
+  })
+})
+
+describe('Test Toggle using React Testing Library', () => {
+  afterEach(cleanup)
+
+  it('render Button component', () => {
+    const { getByText, queryByText } = render(<Toggle />)
+
+    expect(getByText('Off')).toBeDefined()
+    expect(queryByText('On')).toBeNull()
+
+    fireEvent.click(getByText('Toggle'))
+
+    expect(getByText('On')).toBeDefined()
+    expect(queryByText('Off')).toBeNull()
   })
 })
